9de0ed2a02c4a27d775e3b2ff4c68a7445e4114a
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-05-03 11:49:38 +0200

    Initial commit

---
+++ src/main/java/com/example/sprintwave/SprintWaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprintwave/SprintWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
