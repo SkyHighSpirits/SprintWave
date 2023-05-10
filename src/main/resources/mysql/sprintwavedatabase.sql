1e404f3f82092d6fdde424ba778a5e5452a5660e
Xarantaur <115454990+Xarantaur@users.noreply.github.com>
2023-05-10 09:08:44 +0200

    MYSQL DATABASE UPDATE
    Update database with project table and test values

--- src/main/resources/mysql/sprintwavedatabase.sql
+++ src/main/resources/mysql/sprintwavedatabase.sql
@@ -4,6 +4,8 @@ USE sprintwavedatabase;
 
 DROP TABLE IF EXISTS workspaces;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS projects;
+
 
 CREATE TABLE workspaces(
                            workspace_id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -21,6 +23,15 @@ CREATE TABLE users(
                       FOREIGN KEY(workspace_id) REFERENCES workspaces(workspace_id)
 );
 
+CREATE TABLE projects(
+                         project_id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                         project_name VARCHAR(255) NOT NULL,
+                         project_owner VARCHAR(225) NOT NULL,
+                         project_status BOOLEAN NOT NULL DEFAULT TRUE,
+                         project_deadline DATE NOT NULL,
+                         workspace_id INT NOT NULL,
+                         FOREIGN KEY(workspace_id) REFERENCES workspaces(workspace_id)
+);
 
 
 INSERT into workspaces (workspace_name)
@@ -28,3 +39,6 @@ VALUES ('KEA');
 
 INSERT INTO users (email, user_password, firstname, lastname, permission_level, workspace_id)
 VALUES ('steffen@localhost.com', 'e10adc3949ba59abbe56e057f20f883e', 'Steffen', 'Andersen', 'ADMINISTRATOR',1);
+
+INSERT INTO projects (project_name, project_owner, project_status, project_id, workspace_id, project_deadline)
+VALUES('testProject','tommy', true, 1, 1, '2023-05-30');
