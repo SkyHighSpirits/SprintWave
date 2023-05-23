9d55e869b0a2637c1b7d213d0fdf6396dc3bb695
Lasse Lysskov <56248206+Lasse11551@users.noreply.github.com>
2023-05-23 07:59:03 +0200

    added two junittest

--- src/test/java/com/example/sprintwave/utility/DataHandlerTest.java
+++ src/test/java/com/example/sprintwave/utility/DataHandlerTest.java
@@ -1,5 +1,6 @@
 package com.example.sprintwave.utility;
 
+import com.example.sprintwave.model.User;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,5 +10,27 @@ class DataHandlerTest {
     @Test
     void limitPointstester() {
         assertEquals(1, DataHandler.limitPoints(-100));
+    }
+
+    @Test
+    void checkUserInformationMatchtester() {
+        User user = new User();
+        user.setEmail("localhost@localhost.com");
+        user.setUser_password("123456");
+        String enteredEmail = "localhost@localhost.com";
+        String enteredPassword = "123456";
+
+        assertTrue(DataHandler.checkUserInformationMatch(user, enteredEmail, enteredPassword));
+    }
+
+    @Test
+    void checkUserInformationMatchtesternumbertwo() {
+        User user = new User();
+        user.setEmail("localhost@localhost.com");
+        user.setUser_password("123456");
+        String enteredEmail = "bobo";
+        String enteredPassword = "bibi";
+
+        assertFalse(DataHandler.checkUserInformationMatch(user, enteredEmail, enteredPassword));
     }
 }
